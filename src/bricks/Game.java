cecc9623c9dd0f1d25523bec408b85ec31fffa86
lightbanes <m.osiak46@gmail.com>
2019-01-05 18:17:19 +0100

    let's call it THE FIRST VERSION

--- src/bricks/Game.java
+++ src/bricks/Game.java
@@ -6,13 +6,38 @@ import java.io.InputStreamReader;
 import java.util.Set;
 
 public class Game {
+	public static void getNextMove(BufferedReader reader, Board board) throws IOException {
+		String read = reader.readLine();
+		if(read == "start")
+			return;
+		String[] line = read.split(",");
+		int x1 = Integer.parseInt(line[0].replace("{", ""));
+		int y1 = Integer.parseInt(line[1].replace("}", ""));
+		int x2 = Integer.parseInt(line[2].replace("{", ""));
+		int y2 = Integer.parseInt(line[3].replace("}", ""));
+		board.board[y1][x1].setState();
+		board.board[y2][x2].setState();
+	}
+	
+	public static void generateNextMove(Board board, Set<Brick> moves) {
+		for(Brick b : moves) {
+			if(b.getS1().getState() == State.FULL || b.getS2().getState() == State.FULL) {
+				continue;
+			}
+			b.getS1().setState();
+			b.getS2().setState();
+			System.out.println(b);
+			break;
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(reader.readLine()); // get size of the board
 		Board myBoard = new Board(n);
 		System.out.println("ok\n");
-		System.out.println(myBoard.toString()); // clean board
+		// TEST System.out.println(myBoard.toString()); // clean board
 		
 		String[] line = reader.readLine().split(","); // get coordinates of occupied squares
 		for(int i = 0; i < line.length; i += 2) {
@@ -21,10 +46,17 @@ public class Game {
 			myBoard.board[y][x].setState();
 		}
 		System.out.println("ok\n");
-		System.out.println(myBoard.toString()); // board with occupied squares
+		
+		
+		// TEST System.out.println(myBoard.toString()); // board with occupied squares
 		
 		Set<Brick> availableMoves = myBoard.getAllAvailableMoves();
-		System.out.println("AVAILABLE MOVES: " + availableMoves.size());
+		// TEST System.out.println("AVAILABLE MOVES: " + availableMoves.size());
+		
+		while(true) {
+			Game.getNextMove(reader, myBoard);
+			Game.generateNextMove(myBoard, availableMoves);
+		}
 		
 		/*
 		 * TO DO: main loop of the game:
